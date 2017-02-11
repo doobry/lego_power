0e1ed8574a2292d74475f7bb4cbbaf709f359dc4
Chris Beswick <chris.beswick@gmail.com>
2017-02-11 14:06:43 +0000

    Add simple interupt driven example

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,56 @@
 #include <Arduino.h>
-#include <LegoPower.h>
 
-LegoPower legoPower;
+#define SERIAL_PIN 2
+#define SERIAL_RATE 76000
+#define SERIAL_MARK 0b1111111101010101
 
-void setup() {
+#define uS_LOW_BIT 421
+#define uS_HIGH_BIT 711
+#define uS_STARTSTOP_BIT 1184
+#define uS_PAUSE 80000
+
+#define bitValue(value, bit) ((value << bit) & 0x8000) == 0x8000
+
+volatile uint32_t _nextInterupt;
+
+volatile uint16_t _message = 0b0000010001001111;
+volatile int8_t _message_index = -2;
+
+ICACHE_RAM_ATTR void timer0_callback () {
+  Serial1.write(SERIAL_MARK);
+  uint32_t nextDelay;
+  _message_index++;
+  if(_message_index == -1 || _message_index == 16) {
+    nextDelay = uS_STARTSTOP_BIT;
+  } else
+  if(_message_index == 17) {
+    _message_index = -2;
+    nextDelay = uS_PAUSE;
+  } else
+  if(bitValue(_message, _message_index)) {
+    nextDelay = uS_HIGH_BIT;
+  } else {
+    nextDelay = uS_LOW_BIT;
+  }
+  _nextInterupt += microsecondsToClockCycles(nextDelay);
+  timer0_write(_nextInterupt);
 }
 
-void sendAndWait(uint8_t command) {
-  legoPower.sendSinglePwm(LEGO_CHANNEL_1, LEGO_OUTPUT_A, command);
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(SERIAL_RATE);
+  timer0_isr_init();
+  _nextInterupt = ESP.getCycleCount();
+  timer0_callback();
+  timer0_attachInterrupt(timer0_callback);
 }
 
 void loop() {
-  sendAndWait(LEGO_PWM_FWD1);
-  sendAndWait(LEGO_PWM_FWD2);
-  sendAndWait(LEGO_PWM_FWD3);
-  sendAndWait(LEGO_PWM_FWD4);
-  sendAndWait(LEGO_PWM_FWD5);
-  sendAndWait(LEGO_PWM_FWD6);
-  sendAndWait(LEGO_PWM_FWD7);
-  sendAndWait(LEGO_PWM_FWD6);
-  sendAndWait(LEGO_PWM_FWD5);
-  sendAndWait(LEGO_PWM_FWD4);
-  sendAndWait(LEGO_PWM_FWD3);
-  sendAndWait(LEGO_PWM_FWD2);
-  sendAndWait(LEGO_PWM_FWD1);
-  sendAndWait(LEGO_PWM_FLOAT);
-  sendAndWait(LEGO_PWM_BWD1);
-  sendAndWait(LEGO_PWM_BWD2);
-  sendAndWait(LEGO_PWM_BWD3);
-  sendAndWait(LEGO_PWM_BWD4);
-  sendAndWait(LEGO_PWM_BWD5);
-  sendAndWait(LEGO_PWM_BWD6);
-  sendAndWait(LEGO_PWM_BWD7);
-  sendAndWait(LEGO_PWM_BWD6);
-  sendAndWait(LEGO_PWM_BWD5);
-  sendAndWait(LEGO_PWM_BWD4);
-  sendAndWait(LEGO_PWM_BWD3);
-  sendAndWait(LEGO_PWM_BWD2);
-  sendAndWait(LEGO_PWM_BWD1);
-  sendAndWait(LEGO_PWM_BREAK);
-  delay(900);
+  /*Serial1.write(SERIAL_MARK);
+  uint32_t nextDelay = getNextDelay();
+  if(nextDelay == uS_PAUSE) {
+    delay(80);
+  } else {
+    delayMicroseconds(nextDelay);
+  }*/
 }
